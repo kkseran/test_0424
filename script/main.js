7b541868c04a11b7e7bc5365aa8475bbb9b0241d
kkseran <seran97@gmail.com>
2024-04-24 14:30:24 +0900

    script 폴더 생성 후 main.js 추가

--- script/main.js
+++ script/main.js
@@ -1,10 +1,8 @@
-// 변수설정
 let btnCall = document.querySelector('.btnCall');
-let menuMo = document.querySelector('.menuMo');
+let menuMo = document.querySelector('manuMo');
 
 btnCall.addEventListener('click', (e) => {
 	e.preventDefault();
-	// a태그가 가지고있는 기본 이벤트를 막아줌(링크이동X)
 	btnCall.classList.toggle('on');
 	menuMo.classList.toggle('on');
 });
